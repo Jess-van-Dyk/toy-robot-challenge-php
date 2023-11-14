25b52bec94c6eba648b628cc60e0fae64c612797
Jess van Dyk <111473430+Jess-van-Dyk@users.noreply.github.com>
2023-11-14 16:20:04 +1000

    updated with more rest scenarios and improved error handling

--- ToyRobot.php
+++ ToyRobot.php
@@ -5,19 +5,21 @@
         private $x;
         private $y;
         private $facing;
-        private $tableWidth = 5;
-        private $tableHeight = 5;
         private $directions = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
 
+        public const TABLE_WIDTH = 5;
+        public const TABLE_HEIGHT = 5;
+
+        public const NOT_PLACED = -1;
         
         public function __construct() {
-            // Within the constructor, the robot has not been placed on the table yet. 
-            $this->x = -1; 
-            $this->y = -1;
+            // Within the constructor, the robot has not been placed on the table yet.
+            $this->x = self::NOT_PLACED; 
+            $this->y = self::NOT_PLACED;
             $this->facing = '';
         }
 
-        public function place($x, $y, $facing) {
+        public function placeRobot($x, $y, $facing) {
             if ($this->isValidPosition($x, $y) && in_array($facing, $this->directions)) {
                 $this->x = $x;
                 $this->y = $y;
@@ -25,43 +27,53 @@
             }
         }
 
-        public function move() {
-            if ($this->isPlaced()) {
-                $newX = $this->x;
-                $newY = $this->y;
+        public function moveRobot() {
+        if (!$this->isPlaced()) {
+            return "Invalid Move. The Robot is not placed on the table.";
+        } else {
+            $newX = $this->x;
+            $newY = $this->y;
 
-                switch ($this->facing) {
-                    case 'NORTH':
-                        $newY++;
-                        break;
-                    case 'EAST':
-                        $newX++;
-                        break;
-                    case 'SOUTH':
-                        $newY--;
-                        break;
-                    case 'WEST':
-                        $newX--;
-                        break;
-                }
-
-                if ($this->isValidPosition($newX, $newY)) {
-                    $this->x = $newX;
-                    $this->y = $newY;
-                }
-            }
+            switch ($this->facing) {
+                case 'NORTH':
+                    $newY++;
+                    break;
+                case 'EAST':
+                    $newX++;
+                    break;
+                case 'SOUTH':
+                    $newY--;
+                    break;
+                case 'WEST':
+                    $newX--;
+                    break;
         }
 
-        public function left() {
-            if ($this->isPlaced()) {
+        if (!$this->isValidPosition($newX, $newY)) {
+            return "Silly Robot..... You Are Going To Fall.";
+        } else {
+            $this->x = $newX;
+            $this->y = $newY;
+        }
+    }
+}
+
+        public function leftTurnRobot() {
+            // turns the direction the robot is facing 90 degrees left.
+            if (!$this->isPlaced()) {
+                return "Invalid Turn. Robot is not placed on the table.";
+            } else {
                 $currentDirection = array_search($this->facing, $this->directions);
                 $newDirection = ($currentDirection + 3) % 4;
                 $this->facing = $this->directions[$newDirection];
             }
         }
 
-        public function right() {
-            if ($this->isPlaced()) {
+        public function rightTurnRobot() {
+            // turns the direction the robot is facing 90 degrees right.
+            if (!$this->isPlaced()) {
+                return "Invalid Turn. Robot is not placed on the table.";
+            } else {
                 $currentDirection = array_search($this->facing, $this->directions);
                 $newDirection = ($currentDirection + 1) % 4;
                 $this->facing = $this->directions[$newDirection];
@@ -69,16 +81,21 @@
         }
 
         public function report() {
-            if ($this->isPlaced()) {
-                echo "{$this->x},{$this->y},{$this->facing}\n";
+            //  Output of where robot is sitting on the table when called.
+            if (!$this->isPlaced()) {
+                return "No Report Avaliable. Robot is not placed on the table.";
+            } else {
+                return "{$this->x},{$this->y},{$this->facing}";
             }
         }
 
         private function isValidPosition($x, $y) {
-            return $x >= 0 && $x < $this->tableWidth && $y >= 0 && $y < $this->tableHeight;
+            // Checks if x & y are within the table width (less than 4 but greater than 0)
+            return $x >= 0 && $x < self::TABLE_WIDTH && $y >= 0 && $y < self::TABLE_HEIGHT;
         }
 
         private function isPlaced() {
-            return $this->x !== -1 && $this->y !== -1;
+            // Checks if the robot has been placed on the table. Coordinates will be between 0-4)
+            return $this->x !== self::NOT_PLACED && $this->y !== self::NOT_PLACED;
         }
     }
--- ToyRobotTest.php
+++ ToyRobotTest.php
@@ -2,23 +2,64 @@
 
 require_once 'ToyRobot.php';
 
-// Test cases
+function testScenarioOne()
+{
     $robot = new ToyRobot();
-
-    $robot->place(0, 0, 'NORTH');
-    $robot->move();
-    $robot->report(); // Output: 0,1,NORTH
+    $robot->placeRobot(0, 0, 'NORTH');
+    $robot->moveRobot();
+    $result = $robot->report(); 
     
+    echo ($result === '0,1,NORTH') ? "Test 1: Pass\n" : "Test 1: Fail\n";
+}
 
-
-
-    $robot->place(0, 0, 'NORTH');
-    $robot->left();
-    $robot->report(); // Output: 0,0,WEST
+function testScenarioTwo()
+{
+    $robot = new ToyRobot();
+    $robot->placeRobot(0, 0, 'NORTH');
+    $robot->leftTurnRobot();
+    $result = $robot->report(); 
     
-    $robot->place(1, 2, 'EAST');
-    $robot->move();
-    $robot->move();
-    $robot->left();
-    $robot->move();
-    $robot->report(); // Output: 3,3,NORTH
+    echo ($result === '0,0,WEST') ? "Test 2: Pass\n" : "Test 2: Fail\n";
+}
+
+function testScenarioThree()
+{
+    $robot = new ToyRobot();
+    $robot->placeRobot(1, 2, 'EAST');
+    $robot->moveRobot();
+    $robot->moveRobot();
+    $robot->leftTurnRobot();
+    $robot->moveRobot();;
+    $result = $robot->report();
+   
+    echo ($result === '3,3,NORTH') ? "Test 3: Pass\n" : "Test 3: Fail\n";
+}
+
+function testScenarioFourFail()
+{
+    
+    $robot = new ToyRobot();
+    $robot->moveRobot();
+    $robot->moveRobot();
+    $robot->leftTurnRobot();
+    $robot->moveRobot();;
+    $result = $robot->report();
+
+    echo ($result === "No Report Avaliable. Robot is not placed on the table.") ? "Test 4: Pass\n" : "Test 4: Fail\n";
+}
+
+function testScenarioFiveFails()
+{
+    $robot = new ToyRobot();
+    $robot->placeRobot(4, 4, 'NORTH');
+    $result = $robot->moveRobot();
+
+    echo ($result === 'Silly Robot..... You Are Going To Fall.') ? "Test 5: Pass\n" : "Test 5: Fail\n";
+}    
+
+
+testScenarioOne();
+testScenarioTwo();
+testScenarioThree();
+testScenarioFourFail();
+testScenarioFiveFails();
